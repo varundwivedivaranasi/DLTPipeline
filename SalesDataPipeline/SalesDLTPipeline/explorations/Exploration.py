4959f5f961390a7218d6a260e72fc2d9c81b028c
varundwivedivaranasi <varundwivedivaranasi>
2025-08-04 17:14:25 +0000

    explore

---
+++ SalesDataPipeline/SalesDLTPipeline/explorations/Exploration.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SELECT * FROM workspace.sales.dim_customers;
+# MAGIC -- SELECT * FROM workspace.sales.dim_products;
+# MAGIC -- SELECT * FROM workspace.sales.fact_sales;
+# MAGIC -- SELECT * FROM workspace.sales.mv_sales_by_region_and_categories;
+# MAGIC -- SELECT * FROM workspace.sales.salesdltpipeline;
+# MAGIC -- SELECT * FROM workspace.sales.silver_customers;
+# MAGIC -- SELECT * FROM workspace.sales.silver_products;
+# MAGIC -- SELECT * FROM workspace.sales.silver_sales;
+# MAGIC -- SELECT * FROM workspace.sales.stg_customers;
+# MAGIC -- SELECT * FROM workspace.sales.stg_products;
+# MAGIC -- SELECT * FROM workspace.sales.stg_sales;
--- SalesDataPipeline/SalesDLTPipeline/explorations/sample_exploration.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-# !!! Before performing any data analysis, make sure to run the pipeline to materialize the sample datasets. The tables referenced in this notebook depend on that step.
-
-display(spark.sql("SELECT * FROM workspace.default.sample_trips_salesdltpipeline"))
